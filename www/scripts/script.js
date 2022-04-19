538fee1b85e1635e82a40976c2c0e971a4576194
Ayubeey <aubsattorov@gmail.com>
2022-04-19 07:15:06 +0500

    section-widget

--- www/scripts/script.js
+++ www/scripts/script.js
@@ -11,4 +11,41 @@ $(document).ready(function(){
       });
 
 
+    $('.ClimbingGear').on('click', function(){
+
+        $('#ClimbingGear').slideToggle();
+      });
+
+
+    $('.HikingGear').on('click', function(){
+
+        $('#HikingGear').slideToggle();
+      });
+
+
+    $('.Headwear').on('click', function(){
+
+        $('#Headwear').slideToggle();
+      });
+
+
+    $('.Apparel').on('click', function(){
+
+        $('#Apparel').slideToggle();
+      });
+
+
+    $('.Jackets').on('click', function(){
+
+        $('#Jackets').slideToggle();
+      });
+
+
+    $('.widget-btn').on('click', function(){
+
+        $('.widget-icons').slideToggle();
+      });
+
+
+
 });
